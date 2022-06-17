85e0ef4dc2dc8c652ae3c770b0941b8a8f9335c6
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-17 18:27:54 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  var form =document.querySelector('#create-task-form').addEventListener('submit',(e)=>
+  {
+
+    e.preventDefault()
+    
+createTodo(e.target.newTaskDescription.value)
+    
+  });
 });
+
+function createTodo(todo){
+  let unorderedList=document.querySelector('#tasks');
+  
+  let creaateOl=document.createElement('li')
+  let remove=document.createElement('button')
+  remove.addEventListener('click',deleteTodo)
+
+  creaateOl.textContent=todo;
+  remove.textContent=" X "
+  creaateOl.appendChild(remove)
+  unorderedList.appendChild(creaateOl)
+
+
+
+}
+
+
+function deleteTodo(e){
+  e.target.parentNode.remove()
+}
